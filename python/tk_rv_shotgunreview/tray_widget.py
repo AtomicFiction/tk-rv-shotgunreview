479831471516e5b75fee6580e73a4cd5d1b1b6ca
Kelsey Charlton <kcharlton@oak-lnx-027.atomicfiction.net>
2018-06-25 17:43:34 -0700

    [[8051]] shot name labels for cuts tray

--- python/tk_rv_shotgunreview/tray_widget.py
+++ python/tk_rv_shotgunreview/tray_widget.py
@@ -1,11 +1,11 @@
 # Copyright (c) 2016 Shotgun Software Inc.
-# 
+#
 # CONFIDENTIAL AND PROPRIETARY
-# 
-# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
 # Source Code License included in this distribution package. See LICENSE.
-# By accessing, using, copying or modifying this work you indicate your 
-# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
 from .ui_tray_widget import Ui_TrayWidget
@@ -15,9 +15,9 @@ import tank
 from tank.platform.qt import QtCore, QtGui
 
 class TrayWidget(QtGui.QWidget):
-    """    
-    This class is typically used in conjunction with a QT View and the 
-    ShotgunDelegate class. 
+    """
+    This class is typically used in conjunction with a QT View and the
+    ShotgunDelegate class.
 
     This has been hacked up at the moment to be just a thumbnail scroller.
     """
@@ -28,44 +28,45 @@ class TrayWidget(QtGui.QWidget):
         Constructor
         """
         QtGui.QWidget.__init__(self, parent)
-       
+
         # set up the UI
-        self.ui = Ui_TrayWidget() 
+        self.ui = Ui_TrayWidget()
         self.ui.setupUi(self)
         # make sure this widget isn't shown  WHY????
         self.setVisible(False)
         self.hintSize = None
         self.parent = parent
 
-        
+
     def set_actions(self, actions):
         """
         Adds a list of QActions to add to the actions menu for this widget.
         """
         # print "TRAY WIDGET set_actions"
         pass
-                                    
+
     def set_selected(self, selected, in_mini_cut=False):
         """
         Adjust the style sheet to indicate selection or not,
-        """        
+        """
         if selected:
             self.ui.thumbnail.setStyleSheet("QLabel { border: 2px solid rgb(40,136,175); }")
         else:
             self.ui.thumbnail.setStyleSheet("QLabel { border: 0px solid rgb(37,38,41); }")
-          
 
-    def set_thumbnail(self, pixmap):
+
+    def set_thumbnail(self, pixmap, shot_name):
         """
         Set a thumbnail given the current pixmap.
         """
         self.ui.thumbnail.setPixmap(pixmap)
+        self.ui.thumb_label.setText(shot_name)
         self.resize(pixmap.size())
         if pixmap.height() < 74:
             self.parent.resize(self.parent.width(), 74)
         else:
             self.parent.resize(self.parent.width(), pixmap.height())
-            
+
     def set_text(self, header, body):
         """
         Populate the lines of text in the widget
@@ -83,7 +84,7 @@ class TrayWidget(QtGui.QWidget):
     # def calculate_size():
     #     """
     #     Calculates and returns a suitable size for this widget.
-    #     """ 
-    #     #return self.ui.thumbnail.size()       
+    #     """
+    #     #return self.ui.thumbnail.size()
     #     return QtCore.QSize(114, 64)
 
--- python/tk_rv_shotgunreview/ui_tray_widget.py
+++ python/tk_rv_shotgunreview/ui_tray_widget.py
@@ -7,7 +7,7 @@ class Ui_TrayWidget(object):
     def setupUi(self, TrayWidget):
         TrayWidget.setObjectName("TrayWidget")
         #TrayWidget.resize(114, 34)
-        
+
         self.horizontalLayout_3 = QtGui.QHBoxLayout(TrayWidget)
         self.horizontalLayout_3.setSpacing(1)
         self.horizontalLayout_3.setContentsMargins(0, 0, 0, 0)
@@ -15,32 +15,43 @@ class Ui_TrayWidget(object):
 
         self.box = QtGui.QFrame(TrayWidget)
         self.box.setObjectName("tray_box")
-        
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)        
+
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
         sizePolicy.setHorizontalStretch(1)
         sizePolicy.setVerticalStretch(0)
-        
+
         # self.box.setSizePolicy(sizePolicy)
 
         self.horizontalLayout_2 = QtGui.QHBoxLayout(self.box)
         self.horizontalLayout_2.setSpacing(1)
         self.horizontalLayout_2.setContentsMargins(2, 0, 2, 0)
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        
+
+        self.thumb_layout = QtGui.QVBoxLayout(self.box)
+
         self.thumbnail = QtGui.QLabel(self.box)
         self.thumbnail.setText("")
         self.thumbnail.setScaledContents(True)
         self.thumbnail.setAlignment(QtCore.Qt.AlignCenter)
         self.thumbnail.setObjectName("thumbnail")
-        
-        self.horizontalLayout_2.addWidget(self.thumbnail)
-        
+
+        # label to hold the shot name for each thumbnail in the cuts tray
+        # (empty in cases where the shot name cannot be determined)
+        self.thumb_label = QtGui.QLabel(self.box)
+        self.thumb_label.setText('')
+        self.thumb_label.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.thumb_layout.addWidget(self.thumbnail)
+        self.thumb_layout.addWidget(self.thumb_label)
+
+        self.horizontalLayout_2.addLayout(self.thumb_layout)
+
         self.horizontalLayout = QtGui.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
-        
+
         self.horizontalLayout_2.addLayout(self.horizontalLayout)
         self.horizontalLayout_3.addWidget(self.box)
 
         QtCore.QMetaObject.connectSlotsByName(TrayWidget)
 
- 
+
